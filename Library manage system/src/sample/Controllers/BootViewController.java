58d6a9160de1013a193167ccedb0889568dc659c
Mukhammad <m.rustamkhonov@student.inha.uz>
2019-12-12 20:00:33 +0500

    some changes added. Books, administrator, student, librarian classes have been added

---
+++ Library manage system/src/sample/Controllers/BootViewController.java
@@ -0,0 +1,25 @@
+package sample.Controllers;
+
+import javafx.fxml.Initializable;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BootViewController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    public void AddBook(ActionEvent event){
+
+    }
+    public void DisplayBook(ActionEvent event){
+
+    }
+
+    public void ModifyBook(ActionEvent event){
+
+    }
+
+}
